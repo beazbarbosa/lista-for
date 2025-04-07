e123bec7e921763bc7cd294cb41fdace60c76495
beazbarbosa <jiglowgguk@gmail.com>
2025-04-06 22:04:08 -0300

    feat (for)

---
+++ exer1.js
@@ -0,0 +1,19 @@
+let cor = 'vermelho'
+
+switch (cor) {
+
+    case cor = 'vermelho':
+        console.log('Pare🤚');
+        break;
+
+    case cor = 'amarelo':
+        console.log('Atenção⚠️');
+        break;
+
+    case cor = 'verde':
+        console.log('Siga🫡');
+        break;
+
+    default:
+        console.log('Cor inválida😒')
+}
